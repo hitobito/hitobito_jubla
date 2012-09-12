42e4a6a66a9fdfed5c1adc9a9cec519130b70790
Pascal Zumkehr <zumkehr@puzzle.ch>
2012-09-12 18:55:14 +0200

    require authorization in controller actions

--- app/models/ability/with_group.rb
+++ app/models/ability/with_group.rb
@@ -27,37 +27,37 @@ class Ability::WithGroup < Ability::Base
     # list people belonging to to the given group
     if user.groups.include?(group)
       # user has any role in this group
-      can :index, Person, group.people.only_public_data
+      can :index, Person, group.people.only_public_data do |p| true end
       can :external, Person
     elsif layers_read.present?
       if layers_read.include?(group.layer_group)
         # user has layer read of the same layer as the group
-        can :index, Person, group.people.only_public_data
+        can :index, Person, group.people.only_public_data do |p| true end
         can :external, Person
       elsif contains_any?(layers_read, group.layer_groups)
         # user has layer read in a above layer of the group
-        can :index, Person, group.people.only_public_data.visible_from_above(group)
+        can :index, Person, group.people.only_public_data.visible_from_above(group) do |p| true end
       end
     elsif user.contact_data_visible?
-      can :index, Person, group.people.only_public_data.contact_data_visible
+      can :index, Person, group.people.only_public_data.contact_data_visible do |p| true end
     end
 
     if layers_read.present?
       if layers_read.include?(group.layer_group)
         # user has layer read of the same layer as the group
-        can :layer_search, Person, Person.only_public_data.in_layer(group.layer_group)
-        can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group)
+        can :layer_search, Person, Person.only_public_data.in_layer(group.layer_group) do |p| true end
+        can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group) do |p| true end
       elsif contains_any?(layers_read, group.layer_groups)
         # user has layer read in a above layer of the group
-        can :layer_search, Person, Person.only_public_data.visible_from_above.in_layer(group.layer_group)
-        can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group)
+        can :layer_search, Person, Person.only_public_data.visible_from_above.in_layer(group.layer_group) do |p| true end
+        can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group) do |p| true end
       end
     elsif user.contact_data_visible?
-      can :layer_search, Person, Person.only_public_data.contact_data_visible.in_layer(group.layer_group)
-      can :deep_search, Person, Person.only_public_data.contact_data_visible.in_or_below(group.layer_group)
+      can :layer_search, Person, Person.only_public_data.contact_data_visible.in_layer(group.layer_group) do |p| true end
+      can :deep_search, Person, Person.only_public_data.contact_data_visible.in_or_below(group.layer_group) do |p| true end
     elsif user.groups.include?(group) # this is last here as it has the least search abilities
-      can :layer_search, Person, Person.only_public_data.in_layer(group.layer_group).where('roles.group_id' => user.groups)
-      can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group).where('roles.group_id' => user.groups)
+      can :layer_search, Person, Person.only_public_data.in_layer(group.layer_group).where('roles.group_id' => user.groups) do |p| true end
+      can :deep_search, Person, Person.only_public_data.visible_from_above.in_or_below(group.layer_group).where('roles.group_id' => user.groups) do |p| true end
     end
     
   end
