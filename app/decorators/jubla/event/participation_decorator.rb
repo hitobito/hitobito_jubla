27bcc74aae0af34f57270602bb1aba7fe59fd6d0
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-02-05 10:19:13 +0100

    adjustments for qualifications, refs #5721

--- app/decorators/jubla/event/participation_decorator.rb
+++ app/decorators/jubla/event/participation_decorator.rb
@@ -9,16 +9,7 @@ module Jubla::Event::ParticipationDecorator
   extend ActiveSupport::Concern
 
   included do
-    alias_method_chain :qualification_link, :status
     alias_method_chain :originating_group, :state
-  end
-
-  def qualification_link_with_status(group)
-    if event.qualification_possible?
-      qualification_link_without_status(group)
-    else
-      h.icon(qualified? ? :ok : :minus)
-    end
   end
 
   def originating_group_with_state
