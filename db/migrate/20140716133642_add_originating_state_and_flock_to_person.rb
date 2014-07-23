df08654f56a69ff03395260c20dff2baa8dd9c48
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-07-23 16:26:37 +0200

    dont autoupdate origins (too slow)

--- db/migrate/20140716133642_add_originating_state_and_flock_to_person.rb
+++ db/migrate/20140716133642_add_originating_state_and_flock_to_person.rb
@@ -9,8 +9,5 @@ class AddOriginatingStateAndFlockToPerson < ActiveRecord::Migration
   def change
     add_column(:people, :originating_state_id, :integer)
     add_column(:people, :originating_flock_id, :integer)
-    say_with_time "Updating #{Person.count} People ..." do
-      Person.includes(:roles).to_a.each { |person| person.update_columns(GroupOriginator.new(person).to_h) }
-    end
   end
 end
