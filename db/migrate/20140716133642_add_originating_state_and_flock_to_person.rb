bf33c225c5a9785ea9ce38b72d6a69188b5e5743
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-07-28 10:45:58 +0200

    update origins during migration

--- db/migrate/20140716133642_add_originating_state_and_flock_to_person.rb
+++ db/migrate/20140716133642_add_originating_state_and_flock_to_person.rb
@@ -6,8 +6,49 @@
 #  https://github.com/hitobito/hitobito_jubla.
 
 class AddOriginatingStateAndFlockToPerson < ActiveRecord::Migration
+  attr_reader :people, :flocks, :states
+
   def change
     add_column(:people, :originating_state_id, :integer)
     add_column(:people, :originating_flock_id, :integer)
+
+    say_with_time("loading people") { load_people }
+
+    say_with_time("compute origins") { compute_origins }
+    say_with_time("writing origins") { write_origins }
+  end
+
+  private
+
+  def load_people
+    groups = GroupOriginator::FLOCK_ROLES + GroupOriginator::STATE_ROLES
+    @people = Person.
+      includes(:roles).
+      where(roles: { type: groups } )
+    say("loaded #{people.size} people")
+  end
+
+  def compute_origins
+    @flocks = Hash.new {|k,v| k[v] = [] }
+    @states = Hash.new {|k,v| k[v] = [] }
+
+    people.each do |person|
+      hash = GroupOriginator.new(person).to_h
+      flock_id = hash[:originating_flock_id]
+      state_id = hash[:originating_state_id]
+
+      flocks[flock_id] << person.id if flock_id
+      states[state_id] << person.id if state_id
+    end
+  end
+
+  def write_origins
+    flocks.each do |flock_id, people_ids|
+      Person.where(id: people_ids).update_all(originating_flock_id: flock_id)
+    end
+
+    states.each do |state_id, people_ids|
+      Person.where(id: people_ids).update_all(originating_state_id: state_id)
+    end
   end
 end
