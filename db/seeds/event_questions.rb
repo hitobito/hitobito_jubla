b981a5ffd6a7cba23812542cbb8404c1b8203c6c
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-07-28 16:11:29 +0200

    moved migration from core to wagon

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -6,12 +6,12 @@
 #  https://github.com/hitobito/hitobito_jubla.
 
 Event::Question.seed(:event_id, :question,
-  {question: 'Ich habe folgendes ÖV Abo',
+  {question: 'Ich habe während dem Kurs folgendes ÖV Abo',
    choices: 'GA, Halbtax / unter 16, keine Vergünstigung'},
-  
+
   {question: 'Ich bin Vegetarier',
    choices: 'ja, nein'},
-   
+
   {question: 'Ich habe bereits den Schub (das Werkbuch für Leiterinnen und Leiter der Jubla)',
    choices: 'ja, nein'},
 )
