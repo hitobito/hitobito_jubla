485b257c680476f688f3a4dc73f1d20ee4a017e5
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-09-04 14:15:18 +0200

    sort participations by originating flocks and states, fixes #7304

---
+++ app/controllers/jubla/event/participations_controller.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito_jubla and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jubla.
+
+module Jubla::Event::ParticipationsController
+  extend ActiveSupport::Concern
+
+  # both originating_state and originating_flock reference a group
+  #
+  # when associations are included in participation_filter AR uses
+  # the groups table name for the first included association (flocks)
+  # and aliases the table name for the second association (states)
+  included do
+    self.sort_mappings_with_indifferent_access.merge!(originating_state: null_safe_sort('originating_states_people.name'))
+    self.sort_mappings_with_indifferent_access.merge!(originating_flock: null_safe_sort('groups.name'))
+  end
+
+end
