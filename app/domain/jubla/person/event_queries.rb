1b81ae28c44be3ba345852596c19a07da66d2665
Dinuu <martin@zeilenwerk.ch>
2016-11-16 09:22:02 +0100

    Issue #103, add event list to person view if coach

---
+++ app/domain/jubla/person/event_queries.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+module Jubla
+  module Person::EventQueries
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain :upcoming_events, :coached
+    end
+
+    def coached_events
+      person.events.
+        up_to_a_month_ago.
+        merge(::Event::Participation.active).
+        joins(participations: :roles).
+        where(event_roles: { type: [::Event::Camp::Role::Coach.sti_name,
+                                    ::Event::Course::Role::Advisor.sti_name] }).
+        uniq.
+        includes(:groups).
+        preload_all_dates.
+        order_by_date
+    end
+
+    def upcoming_events_with_coached
+      upcoming_events_without_coached.
+        joins('INNER JOIN event_roles ON event_roles.participation_id = event_participations.id').
+        where.not(event_roles: { type: [::Event::Camp::Role::Coach.sti_name,
+                                        ::Event::Course::Role::Advisor.sti_name] })
+    end
+
+  end
+end
