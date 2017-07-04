1e646e68f59275b68b33d622c9ebd27549ab3381
Pascal Simon <simon@puzzle.ch>
2017-07-04 15:42:22 +0200

    update jubla specific event questions, hitobito/hitobito#320

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -1,10 +1,14 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
 
+# delete all default event questions first
+Event::Question.where(event_id: nil).destroy_all
+
+# recreate default event questions
 Event::Question.seed(:event_id, :question,
   {question: 'Ich habe während dem Kurs folgendes ÖV Abo',
    choices: 'GA, Halbtax / unter 16, keine Vergünstigung'},
@@ -12,6 +16,9 @@ Event::Question.seed(:event_id, :question,
   {question: 'Ich bin Vegetarier',
    choices: 'ja, nein'},
 
-  {question: 'Ich habe bereits den Schub (das Werkbuch für Leiterinnen und Leiter der Jubla)',
+  {question: 'Ich habe bereits das neue Kurshilfsmittel «schub» (Auflage 2016)',
+   choices: 'ja, nein'},
+
+  {question: 'Ich habe bereits ein Meisterwerk (das Handbuch der Mindestkenntnisse)',
    choices: 'ja, nein'},
 )
