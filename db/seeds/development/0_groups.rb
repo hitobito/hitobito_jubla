617b8aca50bf27cec01d2d8a6033472aa88c29eb
Janiss Binder <binder@puzzle.ch>
2017-09-06 12:13:33 +0200

    fix some rubocop warnings

--- db/seeds/development/0_groups.rb
+++ db/seeds/development/0_groups.rb
@@ -24,65 +24,63 @@ end
 
 
 Group::FederalWorkGroup.seed(:name, :parent_id,
-  {name: 'AG Bundeslager',
-   parent_id: ch.id },
-)
+                             name: 'AG Bundeslager',
+                             parent_id: ch.id)
 
 states = Group::State.seed(:name, :parent_id,
-  {name: 'Kanton Bern',
-   short_name: 'BE',
-   address: "Klostergasse 3",
-   zip_code: "3333",
-   town: "Bern",
-   country: "Svizzera",
-   email: "bern@be.ch",
-   parent_id: ch.id},
+                           { name: 'Kanton Bern',
+                             short_name: 'BE',
+                             address: 'Klostergasse 3',
+                             zip_code: '3333',
+                             town: 'Bern',
+                             country: 'Svizzera',
+                             email: 'bern@be.ch',
+                             parent_id: ch.id },
 
-  {name: 'Kanton Zürich',
-   short_name: 'ZH',
-   address: "Tellgasse 3",
-   zip_code: "8888",
-   town: "Zürich",
-   country: "Svizzera",
-   email: "zuerich@zh.ch",
-   parent_id: ch.id },
+                           { name: 'Kanton Zürich',
+                             short_name: 'ZH',
+                             address: 'Tellgasse 3',
+                             zip_code: '8888',
+                             town: 'Zürich',
+                             country: 'Svizzera',
+                             email: 'zuerich@zh.ch',
+                             parent_id: ch.id },
 
-  {name: 'Kanton Nordost',
-   short_name: 'NO',
-   address: "Nordostgasse 3",
-   zip_code: "9000",
-   town: "Nordosthausen",
-   country: "Svizzera",
-   email: "nordost@nordost.ch",
-   parent_id: ch.id },
+                           { name: 'Kanton Nordost',
+                             short_name: 'NO',
+                             address: 'Nordostgasse 3',
+                             zip_code: '9000',
+                             town: 'Nordosthausen',
+                             country: 'Svizzera',
+                             email: 'nordost@nordost.ch',
+                             parent_id: ch.id },
 
-  {name: 'Kanton Luzern',
-   short_name: 'LU',
-   address: "Kramgasse 3",
-   zip_code: "4000",
-   town: "Luzern",
-   country: "Svizzera",
-   email: "luzern@lu.ch",
-   parent_id: ch.id },
+                           { name: 'Kanton Luzern',
+                             short_name: 'LU',
+                             address: 'Kramgasse 3',
+                             zip_code: '4000',
+                             town: 'Luzern',
+                             country: 'Svizzera',
+                             email: 'luzern@lu.ch',
+                             parent_id: ch.id },
 
-  {name: 'Kanton Schaffhausen',
-   short_name: 'SH',
-   address: "Hauptstrasse 3",
-   zip_code: "9500",
-   town: "Stein",
-   country: "Svizzera",
-   email: "stein@jubla.ch",
-   parent_id: ch.id },
+                           { name: 'Kanton Schaffhausen',
+                             short_name: 'SH',
+                             address: 'Hauptstrasse 3',
+                             zip_code: '9500',
+                             town: 'Stein',
+                             country: 'Svizzera',
+                             email: 'stein@jubla.ch',
+                             parent_id: ch.id },
 
-  {name: 'Kanton Unterwalden',
-   short_name: 'UW',
-   address: "Hohle Gasse 3",
-   zip_code: "4600",
-   town: "Stans",
-   country: "Schweiz",
-   email: "uw@tell.ch",
-   parent_id: ch.id },
-)
+                           name: 'Kanton Unterwalden',
+                           short_name: 'UW',
+                           address: 'Hohle Gasse 3',
+                           zip_code: '4600',
+                           town: 'Stans',
+                           country: 'Schweiz',
+                           email: 'uw@tell.ch',
+                           parent_id: ch.id)
 
 states.each do |s|
   seeder.seed_social_accounts(s)
@@ -91,130 +89,123 @@ states.each do |s|
 end
 
 Group::StateProfessionalGroup.seed(:name, :parent_id,
-  {name: 'FG Sicherheit',
-   parent_id: states[0].id },
+                                   { name: 'FG Sicherheit',
+                                     parent_id: states[0].id },
 
-  {name: 'FG Security',
-   parent_id: states[2].id },
-)
+                                   name: 'FG Security',
+                                   parent_id: states[2].id)
 
 Group::StateWorkGroup.seed(:name, :parent_id,
-  {name: 'AG Kantonslager',
-   parent_id: states[0].id },
-)
+                           name: 'AG Kantonslager',
+                           parent_id: states[0].id)
 
 regions = Group::Region.seed(:name, :parent_id,
-  {name: 'Region Stadt',
-   parent_id: states[0].id }.merge(seeder.group_attributes),
+                             { name: 'Region Stadt',
+                               parent_id: states[0].id }.merge(seeder.group_attributes),
 
-  {name: 'Region Oberland',
-   parent_id: states[0].id }.merge(seeder.group_attributes),
+                             { name: 'Region Oberland',
+                               parent_id: states[0].id }.merge(seeder.group_attributes),
 
-  {name: 'Region Jura',
-   parent_id: states[0].id }.merge(seeder.group_attributes),
+                             { name: 'Region Jura',
+                               parent_id: states[0].id }.merge(seeder.group_attributes),
 
-  {name: 'Region Stadt',
-   parent_id: states[1].id }.merge(seeder.group_attributes),
+                             { name: 'Region Stadt',
+                               parent_id: states[1].id }.merge(seeder.group_attributes),
 
-  {name: 'Region Oberland',
-   parent_id: states[1].id }.merge(seeder.group_attributes),
-)
+                             { name: 'Region Oberland',
+                               parent_id: states[1].id }.merge(seeder.group_attributes))
 
 flocks = Group::Flock.seed(:name, :parent_id,
-  {name: 'Bern',
-   kind: 'Jungwacht',
-   parent_id: regions[0].id },
+                           { name: 'Bern',
+                             kind: 'Jungwacht',
+                             parent_id: regions[0].id },
 
-  {name: 'Muri',
-   kind: 'Blauring',
-   parent_id: regions[0].id },
+                           { name: 'Muri',
+                             kind: 'Blauring',
+                             parent_id: regions[0].id },
 
-  {name: 'Thun',
-   kind: 'Jungwacht',
-   parent_id: regions[1].id },
+                           { name: 'Thun',
+                             kind: 'Jungwacht',
+                             parent_id: regions[1].id },
 
-  {name: 'Interlaken',
-   kind: 'Jubla',
-   parent_id: regions[1].id },
+                           { name: 'Interlaken',
+                             kind: 'Jubla',
+                             parent_id: regions[1].id },
 
-  {name: 'Simmental',
-   kind: 'Jungwacht',
-   parent_id: regions[1].id },
+                           { name: 'Simmental',
+                             kind: 'Jungwacht',
+                             parent_id: regions[1].id },
 
-  {name: 'Biel',
-   kind: 'Blauring',
-   parent_id: regions[2].id },
+                           { name: 'Biel',
+                             kind: 'Blauring',
+                             parent_id: regions[2].id },
 
-  {name: 'Chräis Chäib',
-   kind: 'Jubla',
-   parent_id: regions[3].id },
+                           { name: 'Chräis Chäib',
+                             kind: 'Jubla',
+                             parent_id: regions[3].id },
 
-  {name: 'Wiedikon',
-   kind: 'Jubla',
-   parent_id: regions[3].id },
+                           { name: 'Wiedikon',
+                             kind: 'Jubla',
+                             parent_id: regions[3].id },
 
-  {name: 'Innerroden',
-   kind: 'Blauring',
-   parent_id: states[2].id },
+                           { name: 'Innerroden',
+                             kind: 'Blauring',
+                             parent_id: states[2].id },
 
-  {name: 'Ausserroden',
-   kind: 'Jungwacht',
-   parent_id: states[2].id },
-)
+                           name: 'Ausserroden',
+                           kind: 'Jungwacht',
+                           parent_id: states[2].id)
 
 flocks.each do |s|
   seeder.seed_social_accounts(s)
 end
 
 Group::ChildGroup.seed(:name, :parent_id,
-  {name: 'Asterix',
-   parent_id: flocks[0].id },
+                       { name: 'Asterix',
+                         parent_id: flocks[0].id },
 
-  {name: 'Obelix',
-   parent_id: flocks[0].id },
+                       { name: 'Obelix',
+                         parent_id: flocks[0].id },
 
-  {name: 'Idefix',
-   parent_id: flocks[0].id },
+                       { name: 'Idefix',
+                         parent_id: flocks[0].id },
 
-  {name: 'Mickey',
-   parent_id: flocks[1].id },
+                       { name: 'Mickey',
+                         parent_id: flocks[1].id },
 
-  {name: 'Minnie',
-   parent_id: flocks[2].id },
+                       { name: 'Minnie',
+                         parent_id: flocks[2].id },
 
-  {name: 'Goofy',
-   parent_id: flocks[3].id },
+                       { name: 'Goofy',
+                         parent_id: flocks[3].id },
 
-  {name: 'Donald',
-   parent_id: flocks[4].id },
+                       { name: 'Donald',
+                         parent_id: flocks[4].id },
 
-  {name: 'Gaston',
-   parent_id: flocks[5].id },
+                       { name: 'Gaston',
+                         parent_id: flocks[5].id },
 
-  {name: 'Tim',
-   parent_id: flocks[6].id },
+                       { name: 'Tim',
+                         parent_id: flocks[6].id },
 
-  {name: 'Hadock',
-   parent_id: flocks[7].id },
+                       { name: 'Hadock',
+                         parent_id: flocks[7].id },
 
-  {name: 'Batman',
-   parent_id: flocks[8].id },
+                       { name: 'Batman',
+                         parent_id: flocks[8].id },
 
-  {name: 'Robin',
-   parent_id: flocks[8].id },
+                       { name: 'Robin',
+                         parent_id: flocks[8].id },
 
-  {name: 'Spiderman',
-   parent_id: flocks[9].id },
-
-)
+                       name: 'Spiderman',
+                       parent_id: flocks[9].id)
 
 Group::SimpleGroup.seed(:name, :parent_id,
-  {name: 'Tschutter',
-   parent_id: flocks[0].id },
+                        { name: 'Tschutter',
+                          parent_id: flocks[0].id },
 
-  {name: 'Angestellte',
-   parent_id: states[0].id },
-)
+                        name: 'Angestellte',
+                        parent_id: states[0].id)
 
 
 Group.rebuild!
