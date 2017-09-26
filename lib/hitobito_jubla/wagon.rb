7b78e3f8cdbdd11b2c2184f76948fa82278d878d
Janiss Binder <binder@puzzle.ch>
2017-09-26 15:52:24 +0200

    Fixed BSV Export
    refs hitobito/hitobito#389
    Only export closed courses instead of all

--- lib/hitobito_jubla/wagon.rb
+++ lib/hitobito_jubla/wagon.rb
@@ -72,7 +72,6 @@ module HitobitoJubla
 
       GroupsController.send :include, Jubla::GroupsController
       EventsController.send :include, Jubla::EventsController
-      Event::ListsController.bsv_course_states = Event::Course.possible_states
       Event::ApplicationMarketController.send :include, Jubla::Event::ApplicationMarketController
       Event::QualificationsController.send :include, Jubla::Event::QualificationsController
       Event::RegisterController.send :include, Jubla::Event::RegisterController
