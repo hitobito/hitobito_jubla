1d5abf635438c2a1194eca37fcf2a08d0b6a0263
Matthias Viehweger <viehweger@puzzle.ch>
2017-09-27 13:13:50 +0200

    Make boolean-column only hold two different values

--- db/migrate/20170925070632_add_alumnus_contact_flags_to_people.rb
+++ db/migrate/20170925070632_add_alumnus_contact_flags_to_people.rb
@@ -1,7 +1,7 @@
 class AddAlumnusContactFlagsToPeople < ActiveRecord::Migration
   def change
-    add_column(:people, :contactable_by_federation, :boolean)
-    add_column(:people, :contactable_by_state, :boolean)
-    add_column(:people, :contactable_by_region, :boolean)
+    add_column :people, :contactable_by_federation, :boolean, null: false, default: false
+    add_column :people, :contactable_by_state,      :boolean, null: false, default: false
+    add_column :people, :contactable_by_region,     :boolean, null: false, default: false
   end
 end
