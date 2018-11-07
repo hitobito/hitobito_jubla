372441f9ba97e506cc34389f6fc363b79d6fbfd7
Janiss Binder <binder@puzzle.ch>
2018-11-07 17:16:49 +0100

    Changed Navigation Location
    Due to core changes, the Navigation moved to the left side

--- spec/regressions/event/camp/kinds_controller_spec.rb
+++ spec/regressions/event/camp/kinds_controller_spec.rb
@@ -28,8 +28,8 @@ describe Event::Camp::KindsController, type: :controller do
   describe_action :get, :index do
     it 'main menu admin is active' do
       dom = Capybara::Node::Simple.new(response.body)
-      item = dom.find('body nav ul.nav li', text: 'Einstellungen')
-      expect(item[:class]).to eq('active')
+      item = dom.find('body nav ul.nav-left-list li', text: 'Einstellungen')
+      expect(item[:class]).to include('active')
     end
   end
 
