fa2c56dabc32f6426635475969353af7dd25b6e8
Andreas Maierhofer <a.maierhofer@gmail.com>
2019-05-20 10:41:41 +0200

    Fix failing spec

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -132,7 +132,7 @@ describe EventsController do
 
       it 'lists events' do
         get :index, group_id: group.id, year: 2012
-        expect(assigns(:events)).to eq [event, events(:top_event)]
+        expect(assigns(:events)).to match_array [event, events(:top_event)]
       end
 
       it 'lists courses' do
