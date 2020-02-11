c48836daab849f4c9cb311684203b4493760e0e3
Andreas Maierhofer <a.maierhofer@gmail.com>
2020-02-11 16:01:59 +0100

    Use quote method instead of removed sanitize in migrations

--- db/migrate/20170918125731_create_alumni_roles_in_alumni_groups.rb
+++ db/migrate/20170918125731_create_alumni_roles_in_alumni_groups.rb
@@ -18,7 +18,7 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
   def delete_alumni_group_members_sql
     <<-SQL
     DELETE FROM roles
-    WHERE type IN (#{sanitize(alumni_member_types)})
+    WHERE type IN (#{quote(alumni_member_types)})
     SQL
   end
 
@@ -27,15 +27,15 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
     <<-SQL
     SELECT DISTINCT person_id, layer_group_id FROM roles
     INNER JOIN groups ON roles.group_id = groups.id
-    WHERE roles.type IN (#{sanitize(role_types)})
+    WHERE roles.type IN (#{quote(role_types)})
     AND roles.deleted_at IS NULL
     AND person_id NOT IN (
       SELECT DISTINCT person_id FROM roles
       INNER JOIN groups g ON roles.group_id = g.id
       WHERE g.layer_group_id = layer_group_id
       AND roles.deleted_at IS NULL
-      AND (roles.type NOT IN (#{sanitize(role_types)}) OR
-          roles.type IN (#{sanitize(external_role_types)}))
+      AND (roles.type NOT IN (#{quote(role_types)}) OR
+          roles.type IN (#{quote(external_role_types)}))
     )
     GROUP BY person_id, layer_group_id
     SQL
@@ -44,12 +44,12 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
   def find_groups_sql(layer_group_ids)
     <<-SQL
     SELECT layer_group_id, id, type FROM groups
-    WHERE layer_group_id IN (#{sanitize(layer_group_ids)})
-    AND groups.type IN (#{sanitize(new_alumni_group_types)})
+    WHERE layer_group_id IN (#{quote(layer_group_ids)})
+    AND groups.type IN (#{quote(new_alumni_group_types)})
     SQL
   end
 
-  def insert_roles_sql(list, group_memo, now = Role.sanitize(Time.zone.now))
+  def insert_roles_sql(list, group_memo, now = Role.quote(Time.zone.now))
     <<-SQL
     INSERT INTO roles(created_at, updated_at, person_id, group_id, type)
     VALUES #{values(list, group_memo, now).join(',')}
@@ -59,7 +59,7 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
   def values(list, group_memo, now)
     list.collect do |person_id, layer_group_id, _|
       _, group_id, group_type = group_memo.fetch(layer_group_id)
-      type = Role.sanitize(member(group_type))
+      type = Role.quote(member(group_type))
       "(#{now}, #{now}, #{person_id}, #{group_id}, #{type})"
     end
   end
@@ -72,8 +72,8 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
     [ group_type, "Member" ].join("::").constantize.to_s
   end
 
-  def sanitize(list)
-    list.collect { |item| ActiveRecord::Base.sanitize(item) }.join(',')
+  def quote(list)
+    list.collect { |item| ActiveRecord::Base.connection.quote(item) }.join(',')
   end
 
   def new_alumni_group_types
