ac3599610dfda2d046bb7bf27da11f47322d9f0a
Andreas Maierhofer <maierhofer@puzzle.ch>
2021-01-07 10:54:04 +0100

    Fix course_condition action_text migration

--- db/migrate/20210105194056_migrate_course_conditions_to_action_text.rb
+++ db/migrate/20210105194056_migrate_course_conditions_to_action_text.rb
@@ -3,8 +3,9 @@ class MigrateCourseConditionsToActionText < ActiveRecord::Migration[6.0]
 
   def change
     rename_column :event_conditions, :content, :content_old
+    binding.pry
     Event::Course::Condition.find_each do |cct|
-      cct.update_attribute(:content, simple_format(cct.content))
+      cct.update(content: simple_format(cct.content_old))
     end
     remove_column :event_conditions, :content_old
     Event::Course::Condition.reset_column_information
