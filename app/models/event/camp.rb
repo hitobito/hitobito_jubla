fe8267b6655e9279e27f81574d1eb94590056c41
Nils Rauch <rauch@puzzle.ch>
2022-09-12 09:14:36 +0200

    Use relative date helper methods

--- app/models/event/camp.rb
+++ app/models/event/camp.rb
@@ -65,7 +65,7 @@ class Event::Camp < Event
   belongs_to :kind, class_name: 'Event::Camp::Kind'
 
   scope :in_next, -> (duration) {
-    timespan = Time.zone.now.midnight + duration
+    timespan = duration.from_now.to_date
     where('event_dates.start_at <= ? OR event_dates.finish_at <= ?', timespan, timespan)
   }
 end
