9f29cf588591eb838a2abc173cb51e7775d91fe2
richardjubla <95908537+richardjubla@users.noreply.github.com>
2023-03-07 18:54:12 +0100

    Aktualisierung event_questions.rb

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
@@ -13,14 +13,14 @@ Event::Question.where(event_id: nil).destroy_all
   {question: 'Ich habe während dem Kurs folgendes ÖV Abo',
    choices: 'GA, Halbtax / unter 16, keine Vergünstigung'},
 
-  {question: 'Ich bin Vegetarier/in',
-   choices: 'ja, nein'},
+  {question: 'Ich habe folgende Essgewohnheiten/Allergien',
+   choices: 'Nichts Spezielles, Vegetarisch, Vegan, Glutenfrei, Laktosefrei'},
 
-  {question: 'Ich habe bereits das neue Kurshilfsmittel «schub» (Auflage 2016)',
-   choices: 'ja, nein'},
+  {question: 'Den schub (Ordner mit fünf schub-Broschüren, digital unter www.jubla.ch/schub)...',
+   choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenen analogen Ordner'},
 
-  {question: 'Ich habe bereits ein Meisterwerk (das Handbuch der Mindestkenntnisse)',
-   choices: 'ja, nein'},
+  {question: 'Das meisterwerk (Handbuch der Mindestkenntnisse Jubla-Technik, digital unter www.jubla.ch/jublatechnik)...',
+   choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenes analoges Handbuch'},
 ].each do |attrs|
   eq = Event::Question.find_or_initialize_by(
     event_id: attrs.delete(:event_id),
