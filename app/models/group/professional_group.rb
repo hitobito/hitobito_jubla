3c6d74e6daf976de89ebc8c372426e56d969759c
richardjubla <95908537+richardjubla@users.noreply.github.com>
2023-12-07 11:39:42 +0100

    Anpassungen von Rollen: FG-Mitgleider :contact_data-Flag entfernen

--- app/models/group/professional_group.rb
+++ app/models/group/professional_group.rb
@@ -35,7 +35,7 @@
 #  jubla_property_insurance    :boolean          default(FALSE), not null
 #
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
@@ -47,7 +47,7 @@ class Group::ProfessionalGroup < Group
   end
 
   class Member < Jubla::Role::Member
-    self.permissions = [:contact_data, :group_read]
+    self.permissions = [:group_read]
   end
 
 end
