f2a012bfb64515d4a10cca32746077cf06a454f0
Matthias Viehweger <viehweger@puzzle.ch>
2024-03-06 16:01:33 +0100

    Load seeds as specs need the CustomContents

---
+++ spec/support/load_seeds.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2017-2024, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
+SeedFu.quiet = true
+
+SeedFu.seed [
+  Rails.root.join('db', 'seeds'),
+  HitobitoJubla::Wagon.root.join('db', 'seeds'),
+]
