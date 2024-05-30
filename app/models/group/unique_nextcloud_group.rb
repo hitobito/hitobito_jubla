d183f4e5d139b2726282de7c7a9665e0e2c358e3
Matthias Viehweger <viehweger@puzzle.ch>
2024-05-30 21:58:40 +0200

    Conform to linter and styleguide

--- app/models/group/unique_nextcloud_group.rb
+++ app/models/group/unique_nextcloud_group.rb
@@ -1,18 +1,21 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2023, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
 
 module Group::UniqueNextcloudGroup
-    extend ActiveSupport::Concern
-    
-    included do
-        self.nextcloud_group = :calculate_nextcloud_group
-    end
-    
-    def calculate_nextcloud_group
-        { "gid" => "#{group_id.to_s}_#{self.type}", "displayName" => "#{group.name} - #{self.class.model_name.human(count:2)}" }
-    end
+  extend ActiveSupport::Concern
+
+  included do
+    self.nextcloud_group = :calculate_nextcloud_group
+  end
+
+  def calculate_nextcloud_group
+    {
+      'gid' => "#{group_id}_#{type}",
+      'displayName' => "#{group.name} - #{self.class.model_name.human(count: 2)}"
+    }
+  end
 end
