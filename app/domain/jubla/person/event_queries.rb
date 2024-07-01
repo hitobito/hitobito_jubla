4dea3bdaeaafe97de36a3e8d16c9c994d695d025
megli2 <egli@puzzle.ch>
2024-07-01 13:21:04 +0200

    modify query and tab sql in

--- app/domain/jubla/person/event_queries.rb
+++ app/domain/jubla/person/event_queries.rb
@@ -30,7 +30,7 @@ module Jubla
       upcoming_events_without_coached.
         joins('INNER JOIN event_roles ON event_roles.participation_id = event_participations.id').
         where.not(event_roles: { type: [::Event::Camp::Role::Coach.sti_name,
-                                        ::Event::Course::Role::Advisor.sti_name] })
+                                        ::Event::Course::Role::Advisor.sti_name].map(&:sti_name) })
     end
 
   end
