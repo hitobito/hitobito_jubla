42ecd8a21ce8a19ca911a822210116c10e7afe71
megli2 <egli@puzzle.ch>
2024-07-09 14:27:47 +0200

    modify method to order results in query

--- app/domain/jubla/person/event_queries.rb
+++ app/domain/jubla/person/event_queries.rb
@@ -8,7 +8,7 @@ module Jubla
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :upcoming_events, :coached
+      alias_method_chain :unordered_upcoming_events, :coached
     end
 
     def coached_events
@@ -24,11 +24,12 @@ module Jubla
         .order_by_date
     end
 
-    def upcoming_events_with_coached
-      upcoming_events_without_coached
-        .joins("INNER JOIN event_roles ON event_roles.participation_id = event_participations.id")
+    def unordered_upcoming_events_with_coached
+      unordered_upcoming_events_without_coached
+        .joins('INNER JOIN event_roles ON event_roles.participation_id = event_participations.id')
         .where.not(event_roles: { type: [::Event::Camp::Role::Coach,
-                                        ::Event::Course::Role::Advisor].map(&:sti_name) })
+                                         ::Event::Course::Role::Advisor].map(&:sti_name) })
+        .order(:start_at)
     end
   end
 end
