6780cb1364ef21cf77acf2b5e1c395e1d50f6066
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-04 09:21:16 +0200

    Adjust some ui code and specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,16 +1,12 @@
-# encoding: utf-8
-
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
 
-require 'spec_helper'
+require "spec_helper"
 
 describe Group do
-
-  include_examples 'group types'
-
+  include_examples "group types"
 
   describe Group::Federation do
     subject { Group::Federation }
@@ -31,17 +27,16 @@ describe Group do
     it { is_expected.to have(11).role_types }
     it { is_expected.to be_layer }
 
-    it 'may have same name as other flock with different kind' do
+    it "may have same name as other flock with different kind" do
       parent = groups(:city)
-      flock = Group::Flock.new(name: 'bla', kind: 'Jungwacht')
+      flock = Group::Flock.new(name: "bla", kind: "Jungwacht")
       flock.parent = parent
       flock.save!
-      other = Group::Flock.new(name: 'bla', kind: 'Blauring')
+      other = Group::Flock.new(name: "bla", kind: "Blauring")
       other.parent = parent
       other.valid?
       expect(other.errors.full_messages).to eq([])
     end
-
   end
 
   describe Group::SimpleGroup do
@@ -52,45 +47,44 @@ describe Group do
     it { is_expected.not_to be_layer }
     its(:possible_children) { should include(Group::SimpleGroup) }
 
-    it 'includes the common roles' do
+    it "includes the common roles" do
       expect(subject.role_types).to include(Group::SimpleGroup::GroupAdmin)
     end
 
-    it 'includes the external role' do
+    it "includes the external role" do
       expect(subject.role_types).to include(Group::SimpleGroup::External)
     end
 
-    it 'may have same name as other group with different parent' do
-      flock = Group::SimpleGroup.new(name: 'bla')
+    it "may have same name as other group with different parent" do
+      flock = Group::SimpleGroup.new(name: "bla")
       flock.parent = groups(:city)
       flock.save!
-      other = Group::SimpleGroup.new(name: 'bla')
+      other = Group::SimpleGroup.new(name: "bla")
       other.parent = groups(:bern)
       expect(other).to be_valid
     end
-
   end
 
-  describe '#all_types' do
+  describe "#all_types" do
     subject { Group.all_types }
 
-    it 'must have root as the first item' do
+    it "must have root as the first item" do
       expect(subject.first).to eq(Group::Federation)
     end
 
-    it 'must have simple group as last item' do
+    it "must have simple group as last item" do
       expect(subject.last).to eq(Group::SimpleGroup)
     end
 
-    context '#destroy' do
-      it 'deletes Layer if there are only Alumnus Group childs' do
+    context "#destroy" do
+      it "deletes Layer if there are only Alumnus Group childs" do
         group = Group::Region.all.first
         group.children.delete_all
-        alumnus_group = Group::RegionalAlumnusGroup.create(name: 'test_group', parent_id: group.id)
-        alumnus_group2 = Group::RegionalAlumnusGroup.create(name: 'test_group2',
-                                                            parent_id: group.id)
-        alumnus_group3 = Group::RegionalAlumnusGroup.create(name: 'test_group3',
-                                                            parent_id: alumnus_group2.id)
+        alumnus_group = Group::RegionalAlumnusGroup.create(name: "test_group", parent_id: group.id)
+        alumnus_group2 = Group::RegionalAlumnusGroup.create(name: "test_group2",
+          parent_id: group.id)
+        alumnus_group3 = Group::RegionalAlumnusGroup.create(name: "test_group3",
+          parent_id: alumnus_group2.id)
 
         expect(group.destroy).not_to be false
 
@@ -100,32 +94,32 @@ describe Group do
         expect(Group.without_deleted.where(id: alumnus_group3.id)).not_to exist
       end
 
-      it 'deletes all alumnus roles when deleting an alumnus group via its layer' do
+      it "deletes all alumnus roles when deleting an alumnus group via its layer" do
         group = Group::Region.all.first
         group.children.delete_all
-        alumnus_group = Group::RegionalAlumnusGroup.create(name: 'test_group', parent_id: group.id)
+        alumnus_group = Group::RegionalAlumnusGroup.create(name: "test_group", parent_id: group.id)
         Role.create(group: alumnus_group)
 
-        expect(Role.with_deleted.where.not(group_id: Group.with_deleted.select(:id)).count).to be 0
+        expect(Role.with_inactive.where.not(group_id: Group.with_deleted.select(:id)).count).to be 0
 
-        expect { group.destroy }.not_to change { Role.with_deleted.where.not(group_id: Group.with_deleted.select(:id)).count }
+        expect { group.destroy }.not_to change { Role.with_inactive.where.not(group_id: Group.with_deleted.select(:id)).count }
       end
     end
   end
 
-  describe '.course_offerers' do
+  describe ".course_offerers" do
     subject { Group.course_offerers }
 
-    it 'includes federation' do
+    it "includes federation" do
       is_expected.to include groups(:ch)
     end
 
-    it 'includes states' do
+    it "includes states" do
       is_expected.to include groups(:be)
       is_expected.to include groups(:no)
     end
 
-    it 'does not include flocks' do
+    it "does not include flocks" do
       is_expected.not_to include groups(:thun)
       is_expected.not_to include groups(:ausserroden)
       is_expected.not_to include groups(:innerroden)
@@ -133,29 +127,27 @@ describe Group do
       is_expected.not_to include groups(:muri)
     end
 
-    it 'orders by parent and name' do
-      expected = ['Jubla Schweiz', 'Kanton Bern', 'Nordostschweiz']
+    it "orders by parent and name" do
+      expected = ["Jubla Schweiz", "Kanton Bern", "Nordostschweiz"]
       expect(subject.map(&:name)).to eq expected
     end
   end
 
-  describe 'alumnus filter' do
+  describe "alumnus filter" do
     let(:group) { groups(:ch) }
 
-    it 'creating non alumnus group creats alumnus filter' do
+    it "creating non alumnus group creats alumnus filter" do
       expect do
-        board = Group::FederalBoard.create(name: 'board', parent_id: group.id)
-        filter = board.people_filters.find_by(name: 'Ehemalige')
+        board = Group::FederalBoard.create(name: "board", parent_id: group.id)
+        filter = board.people_filters.find_by(name: "Ehemalige")
         expect(filter.filter_chain).to be_present
-      end.to change { PeopleFilter.count  }.by(1)
+      end.to change { PeopleFilter.count }.by(1)
     end
 
-    it 'creating alumnus group does not create alumnus filter' do
+    it "creating alumnus group does not create alumnus filter" do
       expect do
-        Group::FederalAlumnusGroup.create(name: 'board', parent_id: group.id)
-      end.not_to change { PeopleFilter.count  }
+        Group::FederalAlumnusGroup.create(name: "board", parent_id: group.id)
+      end.not_to change { PeopleFilter.count }
     end
-
   end
-
 end
