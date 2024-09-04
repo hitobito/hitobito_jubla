fee54ce27201eb518a5b5a20763d3b3ed942d680
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-04 10:07:45 +0200

    Adjust seeder for change

--- db/seeds/development/events.rb
+++ db/seeds/development/events.rb
@@ -1,17 +1,13 @@
-# encoding: utf-8
-
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
 
-
-require Rails.root.join('db', 'seeds', 'support', 'event_seeder')
+require Rails.root.join("db", "seeds", "support", "event_seeder")
 
 srand(42)
 
 class JublaEventSeeder < EventSeeder
-
   @@conditions = []
 
   def seed_event(group_id, type)
@@ -35,11 +31,11 @@ class JublaEventSeeder < EventSeeder
   end
 
   def seed_course(values)
-    event = super(values)
+    event = super
 
     event.reload
-    event.condition_id = @@conditions[event.group_ids.first].shuffle.first
-    event.state = Event::Course.possible_states.shuffle.first
+    event.condition_id = @@conditions[event.group_ids.first].sample
+    event.state = Event::Course.possible_states.sample
     application_contact = event.possible_contact_groups.sample
     event.application_contact_id = application_contact.id
     event.save!
@@ -47,7 +43,7 @@ class JublaEventSeeder < EventSeeder
 
   def seed_event_course_conditions(group_id)
     [:tick, :trick, :track].each do |label|
-      data = { group_id: group_id, label: label, content: Faker::Lorem.paragraph(sentence_count: rand(3) + 1) }
+      data = {group_id: group_id, label: label, content: Faker::Lorem.paragraph(sentence_count: rand(1..3))}
       condition = Event::Course::Condition.seed(:group_id, :label, data).first
       @@conditions[group_id] ||= []
       @@conditions[group_id] << condition.id
@@ -57,7 +53,6 @@ class JublaEventSeeder < EventSeeder
   def camp_group_ids
     Group.where(type: Group::Flock.sti_name).pluck(:id)
   end
-
 end
 
 seeder = JublaEventSeeder.new
@@ -78,4 +73,4 @@ seeder.camp_group_ids.each do |group_id|
   end
 end
 
-Event::Participation.update_all(state: 'assigned', active: true)
+Event::Participation.update_all(state: "assigned", active: true)
