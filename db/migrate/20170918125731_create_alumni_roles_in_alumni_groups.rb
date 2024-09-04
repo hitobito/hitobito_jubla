66eb9ba1acaa5a431c2ed0ecb3b1e8f68e63a26b
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-04 08:58:27 +0200

    Adapt migrations to role changes

--- db/migrate/20170918125731_create_alumni_roles_in_alumni_groups.rb
+++ db/migrate/20170918125731_create_alumni_roles_in_alumni_groups.rb
@@ -1,5 +1,4 @@
 class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
-
   def up
     alumni = select_rows(find_people_sql)
     return unless alumni.present?
@@ -23,17 +22,17 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
   end
 
   def find_people_sql
-    role_types =  existing_alumni_role_types - ["Group::ChildGroup::Alumnus"]
+    role_types = existing_alumni_role_types - ["Group::ChildGroup::Alumnus"]
     <<-SQL
     SELECT DISTINCT person_id, layer_group_id FROM roles
-    INNER JOIN #{table('groups')} ON roles.group_id = #{table('groups')}.id
+    INNER JOIN #{table("groups")} ON roles.group_id = #{table("groups")}.id
     WHERE roles.type IN (#{quote(role_types)})
-    AND roles.deleted_at IS NULL
+    AND (roles.end_on IS NULL OR roles.end_on > CURRENT_DATE)
     AND person_id NOT IN (
       SELECT DISTINCT person_id FROM roles
-      INNER JOIN #{table('groups')} g ON roles.group_id = g.id
+      INNER JOIN #{table("groups")} g ON roles.group_id = g.id
       WHERE g.layer_group_id = layer_group_id
-      AND roles.deleted_at IS NULL
+      AND (roles.end_on IS NULL OR roles.end_on > CURRENT_DATE)
       AND (roles.type NOT IN (#{quote(role_types)}) OR
           roles.type IN (#{quote(external_role_types)}))
     )
@@ -43,16 +42,16 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
 
   def find_groups_sql(layer_group_ids)
     <<-SQL
-    SELECT layer_group_id, id, type FROM #{table('groups')}
+    SELECT layer_group_id, id, type FROM #{table("groups")}
     WHERE layer_group_id IN (#{quote(layer_group_ids)})
-    AND #{table('groups')}.type IN (#{quote(new_alumni_group_types)})
+    AND #{table("groups")}.type IN (#{quote(new_alumni_group_types)})
     SQL
   end
 
   def insert_roles_sql(list, group_memo, now = Role.quote(Time.zone.now))
     <<-SQL
     INSERT INTO roles(created_at, updated_at, person_id, group_id, type)
-    VALUES #{values(list, group_memo, now).join(',')}
+    VALUES #{values(list, group_memo, now).join(",")}
     SQL
   end
 
@@ -69,11 +68,11 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
   end
 
   def member(group_type)
-    [ group_type, "Member" ].join("::").constantize.to_s
+    [group_type, "Member"].join("::").constantize.to_s
   end
 
   def quote(list)
-    list.collect { |item| ActiveRecord::Base.connection.quote(item) }.join(',')
+    list.collect { |item| ActiveRecord::Base.connection.quote(item) }.join(",")
   end
 
   def table(name)
@@ -82,51 +81,50 @@ class CreateAlumniRolesInAlumniGroups < ActiveRecord::Migration[4.2]
 
   def new_alumni_group_types
     ["Group::AlumnusGroup",
-     "Group::StateAlumnusGroup",
-     "Group::FederalAlumnusGroup",
-     "Group::FlockAlumnusGroup",
-     "Group::RegionalAlumnusGroup"
-    ]
+      "Group::StateAlumnusGroup",
+      "Group::FederalAlumnusGroup",
+      "Group::FlockAlumnusGroup",
+      "Group::RegionalAlumnusGroup"]
   end
 
   def existing_alumni_role_types
     ["Group::SimpleGroup::Alumnus",
-     "Group::FederalBoard::Alumnus",
-     "Group::OrganizationBoard::Alumnus",
-     "Group::FederalProfessionalGroup::Alumnus",
-     "Group::FederalWorkGroup::Alumnus",
-     "Group::StateAgency::Alumnus",
-     "Group::StateBoard::Alumnus",
-     "Group::State::Alumnus",
-     "Group::StateProfessionalGroup::Alumnus",
-     "Group::StateWorkGroup::Alumnus",
-     "Group::RegionalBoard::Alumnus",
-     "Group::Region::Alumnus",
-     "Group::RegionalProfessionalGroup::Alumnus",
-     "Group::RegionalWorkGroup::Alumnus",
-     "Group::ChildGroup::Alumnus",
-     "Group::Flock::Alumnus",
-     "Group::Federation::Alumnus"]
+      "Group::FederalBoard::Alumnus",
+      "Group::OrganizationBoard::Alumnus",
+      "Group::FederalProfessionalGroup::Alumnus",
+      "Group::FederalWorkGroup::Alumnus",
+      "Group::StateAgency::Alumnus",
+      "Group::StateBoard::Alumnus",
+      "Group::State::Alumnus",
+      "Group::StateProfessionalGroup::Alumnus",
+      "Group::StateWorkGroup::Alumnus",
+      "Group::RegionalBoard::Alumnus",
+      "Group::Region::Alumnus",
+      "Group::RegionalProfessionalGroup::Alumnus",
+      "Group::RegionalWorkGroup::Alumnus",
+      "Group::ChildGroup::Alumnus",
+      "Group::Flock::Alumnus",
+      "Group::Federation::Alumnus"]
   end
 
   def external_role_types
     ["Group::AlumnusGroup::External",
-     "Group::SimpleGroup::External",
-     "Group::FederalBoard::External",
-     "Group::OrganizationBoard::External",
-     "Group::FederalProfessionalGroup::External",
-     "Group::FederalWorkGroup::External",
-     "Group::StateAgency::External",
-     "Group::StateBoard::External",
-     "Group::State::External",
-     "Group::StateProfessionalGroup::External",
-     "Group::StateWorkGroup::External",
-     "Group::RegionalBoard::External",
-     "Group::Region::External",
-     "Group::RegionalProfessionalGroup::External",
-     "Group::RegionalWorkGroup::External",
-     "Group::ChildGroup::External",
-     "Group::Flock::External",
-     "Group::Federation::External"]
+      "Group::SimpleGroup::External",
+      "Group::FederalBoard::External",
+      "Group::OrganizationBoard::External",
+      "Group::FederalProfessionalGroup::External",
+      "Group::FederalWorkGroup::External",
+      "Group::StateAgency::External",
+      "Group::StateBoard::External",
+      "Group::State::External",
+      "Group::StateProfessionalGroup::External",
+      "Group::StateWorkGroup::External",
+      "Group::RegionalBoard::External",
+      "Group::Region::External",
+      "Group::RegionalProfessionalGroup::External",
+      "Group::RegionalWorkGroup::External",
+      "Group::ChildGroup::External",
+      "Group::Flock::External",
+      "Group::Federation::External"]
   end
 end
--- db/migrate/20180613124429_create_missing_alumnus_members.rb
+++ db/migrate/20180613124429_create_missing_alumnus_members.rb
@@ -1,7 +1,8 @@
 class CreateMissingAlumnusMembers < ActiveRecord::Migration[4.2]
   include Rails.application.routes.url_helpers
 
-  def down; end
+  def down
+  end
 
   def up
     missing_alumni = select_rows(find_missing_people_sql)
@@ -16,20 +17,20 @@ class CreateMissingAlumnusMembers < ActiveRecord::Migration[4.2]
   def find_missing_people_sql
     <<-SQL
     SELECT DISTINCT person_id, layer_group_id FROM roles
-    INNER JOIN #{table('groups')} ON roles.group_id = #{table('groups')}.id
+    INNER JOIN #{table("groups")} ON roles.group_id = #{table("groups")}.id
     WHERE roles.type IN (#{quote(role_types)})
-    AND roles.deleted_at IS NOT NULL
+    AND (roles.end_on IS NULL OR roles.end_on > CURRENT_DATE)
     AND person_id NOT IN (
       SELECT DISTINCT person_id FROM roles
-      INNER JOIN #{table('groups')} g ON roles.group_id = g.id
-      WHERE g.layer_group_id = #{table('groups')}.layer_group_id
-      AND roles.deleted_at IS NULL
+      INNER JOIN #{table("groups")} g ON roles.group_id = g.id
+      WHERE g.layer_group_id = #{table("groups")}.layer_group_id
+      AND (roles.end_on IS NULL OR roles.end_on > CURRENT_DATE)
       AND (
         roles.type IN (#{quote(role_types)}) OR
         roles.type IN ('Group::FlockAlumnusGroup::Member', 'Group::ChildGroup::Child'))
     )
     AND layer_group_id NOT IN (
-      SELECT DISTINCT id from #{table('groups')}
+      SELECT DISTINCT id from #{table("groups")}
       WHERE deleted_at IS NOT NULL
     )
     GROUP BY person_id, layer_group_id
@@ -38,9 +39,9 @@ class CreateMissingAlumnusMembers < ActiveRecord::Migration[4.2]
 
   def find_alumni_groups_sql(layer_group_ids)
     <<-SQL
-    SELECT layer_group_id, id FROM #{table('groups')}
+    SELECT layer_group_id, id FROM #{table("groups")}
     WHERE layer_group_id IN (#{quote(layer_group_ids)})
-    AND #{table('groups')}.type = 'Group::FlockAlumnusGroup'
+    AND #{table("groups")}.type = 'Group::FlockAlumnusGroup'
     SQL
   end
 
@@ -48,34 +49,34 @@ class CreateMissingAlumnusMembers < ActiveRecord::Migration[4.2]
     now = Role.quote(Time.zone.now)
     <<-SQL
     INSERT INTO roles(created_at, updated_at, person_id, group_id, type)
-    VALUES #{values(list, alumni_groups, now).join(',')}
+    VALUES #{values(list, alumni_groups, now).join(",")}
     SQL
   end
 
   def values(list, alumnus_group_memo, now)
     list.collect do |person_id, layer_group_id|
       _, group_id = alumnus_group_memo.fetch(layer_group_id)
-      "(#{now}, #{now}, #{person_id}, #{group_id},"\
+      "(#{now}, #{now}, #{person_id}, #{group_id}," \
         " 'Group::FlockAlumnusGroup::Member')"
     end
   end
 
   def role_types
     ["Group::Flock::President",
-     "Group::Flock::Guide",
-     "Group::Flock::Leader",
-     "Group::Flock::Treasurer",
-     "Group::Flock::GroupAdmin",
-     "Group::Flock::DispatchAddress",
-     "Group::Flock::CampLeader",
-     "Group::Flock::Advisor",
-     "Group::ChildGroup::Leader",
-     "Group::ChildGroup::GroupAdmin",
-     "Group::ChildGroup::DispatchAddress"]
+      "Group::Flock::Guide",
+      "Group::Flock::Leader",
+      "Group::Flock::Treasurer",
+      "Group::Flock::GroupAdmin",
+      "Group::Flock::DispatchAddress",
+      "Group::Flock::CampLeader",
+      "Group::Flock::Advisor",
+      "Group::ChildGroup::Leader",
+      "Group::ChildGroup::GroupAdmin",
+      "Group::ChildGroup::DispatchAddress"]
   end
 
   def quote(list)
-    list.collect { |item| ActiveRecord::Base.connection.quote(item) }.join(',')
+    list.collect { |item| ActiveRecord::Base.connection.quote(item) }.join(",")
   end
 
   def table(name)
