44377dcb9ace5b3dac5da09d4603e356e283ee80
Diego Steiner <diego.steiner@u041.ch>
2024-09-18 13:40:58 +0200

    fix: linting

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -7,28 +7,32 @@
 
 Event::Question.seed_global({
   question: 'Ich habe während dem Kurs folgendes ÖV Abo',
-    choices: 'GA, Halbtax / unter 16, keine Vergünstigung',
-    disclosure: :optional,
-    event_type: Event::Course.sti_name,
-    multiple_choices: false })
+  choices: 'GA, Halbtax / unter 16, keine Vergünstigung',
+  disclosure: :optional,
+  event_type: Event::Course.sti_name,
+  multiple_choices: false
+})
 
 Event::Question.seed_global({
   question: 'Ich habe folgende Essgewohnheiten/Allergien',
-    choices: 'Nichts Spezielles, Vegetarisch, Vegan, Glutenfrei, Laktosefrei',
-    disclosure: :optional,
-    event_type: Event::Course.sti_name,
-    multiple_choices: true })
+  choices: 'Nichts Spezielles, Vegetarisch, Vegan, Glutenfrei, Laktosefrei',
+  disclosure: :optional,
+  event_type: Event::Course.sti_name,
+  multiple_choices: true
+})
 
 Event::Question.seed_global({
   question: 'Den schub (Ordner mit fünf schub-Broschüren, digital unter www.jubla.ch/schub)...',
-    choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenen analogen Ordner',
-    disclosure: :optional,
-    event_type: Event::Course.sti_name,
-    multiple_choices: false })
+  choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenen analogen Ordner',
+  disclosure: :optional,
+  event_type: Event::Course.sti_name,
+  multiple_choices: false
+})
 
 Event::Question.seed_global({
   question: 'Das meisterwerk (Handbuch der Mindestkenntnisse Jubla-Technik, digital unter www.jubla.ch/jublatechnik)...',
-    choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenes analoges Handbuch',
-    disclosure: :optional,
-    event_type: Event::Course.sti_name,
-    multiple_choices: false })
+  choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenes analoges Handbuch',
+  disclosure: :optional,
+  event_type: Event::Course.sti_name,
+  multiple_choices: false
+})
