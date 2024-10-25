a39a073df2c287458661f18accf4d77105af5144
Matthias Viehweger <viehweger@puzzle.ch>
2024-10-25 15:22:32 +0200

    Centralize skipping of the alumnus-logic

---
+++ app/models/nejb_role.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024-2024, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito_jubla and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jubla.
+
+class NejbRole < Role
+  module AlumnusFreeRole
+    def skip_alumnus_callback
+      true
+    end
+
+    def alumnus_manager
+      @alumnus_manager ||= Jubla::Role::NullAlumnusManager.new
+    end
+  end
+  include AlumnusFreeRole
+end
---
+++ spec/models/nejb_role_spec.rb
@@ -0,0 +1,34 @@
+#  Copyright (c) 2012-2024, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito_jubla and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jubla.
+
+require "spec_helper"
+
+describe NejbRole do
+  it "is a Role" do
+    expect(subject).to be_a Role
+  end
+
+  it "is a Jubla::Role" do
+    expect(subject).to be_a Jubla::Role
+  end
+
+  it "disables the alumnus-callback" do
+    expect(subject.skip_alumnus_callback).to be_truthy
+  end
+
+  it "uses the NullAlumnusManager" do
+    expect(subject.alumnus_manager).to be_a Jubla::Role::NullAlumnusManager
+  end
+
+  %w[
+    Group::NejbSchar::GroupAdmin
+  ].each do |role|
+    context role do
+      it "is a NeJbRole" do
+        expect(role.safe_constantize.new).to be_a NejbRole
+      end
+    end
+  end
+end
