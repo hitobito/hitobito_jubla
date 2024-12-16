b47af36037d8472cad4fbfb83dd651fc40353073
Matthias Viehweger <viehweger@puzzle.ch>
2024-12-16 09:28:56 +0100

    Send mail for everyone leaving the layer (#179)
    * Only look at current layer for active roles
    
    * Give long line a better name

--- app/jobs/alumni_mail_job.rb
+++ app/jobs/alumni_mail_job.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2017, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2017-2024, Pfadibewegung Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
@@ -12,7 +12,7 @@ class AlumniMailJob < BaseJob
   end
 
   def perform
-    return if person.roles.without_alumnus.any?
+    return if no_more_active_roles?
 
     if group.is_a?(Group::Flock)
       AlumniMailer.new_member_flock(person).deliver_now
@@ -23,8 +23,12 @@ class AlumniMailJob < BaseJob
 
   private
 
+  def no_more_active_roles?
+    person.roles.without_alumnus.roles_in_layer(person.id, group.layer_group_id).any?
+  end
+
   def group
-    Group.find(@group_id)
+    @group ||= Group.find(@group_id)
   end
 
   def person
