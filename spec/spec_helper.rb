62e63eea12687d2a3726be9c2605742ba36edb53
Andreas Maierhofer <maierhofer@puzzle.ch>
2025-02-19 10:04:30 +0100

    Remove fixture_path deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,17 @@
-# encoding: utf-8
-
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito_jubla and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
 
-load File.expand_path('../../app_root.rb', __FILE__)
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+load File.expand_path("../../app_root.rb", __FILE__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 
-require File.join(ENV['APP_ROOT'], 'spec', 'spec_helper.rb')
+require File.join(ENV["APP_ROOT"], "spec", "spec_helper.rb")
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[HitobitoJubla::Wagon.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+Dir[HitobitoJubla::Wagon.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = File.expand_path('../fixtures', __FILE__)
+  config.fixture_paths = [File.expand_path("../fixtures", __FILE__)]
 end
