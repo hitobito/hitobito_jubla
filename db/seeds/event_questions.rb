25ccdec3cd0f180f8458c0c15f091b6b4ee89cdd
Niklas Jäggi <jaeggi@puzzle.ch>
2025-02-20 08:43:30 +0100

    Update event_question disclosure (#116)

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -5,34 +5,37 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_jubla.
 
-Event::Question.seed_global({
-  question: 'Ich habe während dem Kurs folgendes ÖV Abo',
-  choices: 'GA, Halbtax / unter 16, keine Vergünstigung',
-  disclosure: :optional,
-  event_type: Event::Course.sti_name,
-  multiple_choices: false
-})
+# delete all default event questions first
+Event::Question.where(event_id: nil).destroy_all
 
-Event::Question.seed_global({
-  question: 'Ich habe folgende Essgewohnheiten/Allergien',
-  choices: 'Nichts Spezielles, Vegetarisch, Vegan, Glutenfrei, Laktosefrei',
-  disclosure: :optional,
-  event_type: Event::Course.sti_name,
-  multiple_choices: true
-})
+# recreate default event questions
+questions_data = [
+  { question: 'Ich habe während dem Kurs folgendes ÖV Abo',
+    choices: 'GA, Halbtax / unter 16, keine Vergünstigung',
+    disclosure: :required,
+    multiple_choices: false },
 
-Event::Question.seed_global({
-  question: 'Den schub (Ordner mit fünf schub-Broschüren, digital unter www.jubla.ch/schub)...',
-  choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenen analogen Ordner',
-  disclosure: :optional,
-  event_type: Event::Course.sti_name,
-  multiple_choices: false
-})
+  { question: 'Ich habe folgende Essgewohnheiten/Allergien',
+    choices: 'Nichts Spezielles, Vegetarisch, Vegan, Glutenfrei, Laktosefrei',
+    disclosure: :optional,
+    multiple_choices: true },
 
-Event::Question.seed_global({
-  question: 'Das meisterwerk (Handbuch der Mindestkenntnisse Jubla-Technik, digital unter www.jubla.ch/jublatechnik)...',
-  choices: 'besitze ich und nehme ich mit, leihe ich von jemandem aus, habe ich nicht/kann ich nicht ausleihen/möchte ich als eigenes analoges Handbuch',
-  disclosure: :optional,
-  event_type: Event::Course.sti_name,
-  multiple_choices: false
-})
+  { question: 'Den schub (Ordner mit fünf schub-Broschüren, digital unter jubla.ch/schub)...',
+    choices: 'hab ich bereits und nehme ihn mit in den Kurs, leihe ich von jmd aus und nehme ihn mit in den Kurs (je neuer die Auflage, desto besser), habe ich nicht/leihe ich nicht aus, bestelle ich hiermit als Ordner (Kosten: CHF 27.-)',
+    disclosure: :required,
+    multiple_choices: false },
+
+  { question: 'Das meisterwerk (Handbuch der Mindestkenntnisse Jubla-Technik, digital unter jubla.ch/jublatechnik)...',
+    choices: 'besitze ich bereits und nehme es mit in den Kurs (Auflage ab 2017), leihe ich von jemandem aus der Schar aus und nehme es mit in den Kurs (Auflage ab 2017), habe ich nicht und hätte es gerne als eigenes analoges Handbuch (ohne Kosten)',
+    disclosure: :required,
+    multiple_choices: false },
+]
+
+questions_data.each do |attrs|
+  eq = Event::Question.find_or_initialize_by(
+    event_id: attrs.delete(:event_id),
+    question: attrs.delete(:question)
+  )
+  eq.attributes = attrs
+  eq.save!
+end
