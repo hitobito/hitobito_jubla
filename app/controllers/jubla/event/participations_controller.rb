923298c33a240f8e2278c024cc7f651832dedbcc
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-10-29 15:03:41 +0100

    Adapt to core event/participations api change

--- app/controllers/jubla/event/participations_controller.rb
+++ app/controllers/jubla/event/participations_controller.rb
@@ -7,24 +7,32 @@ module Jubla::Event::ParticipationsController
   extend ActiveSupport::Concern
 
   prepended do
-    sort_mappings_with_indifferent_access
-      .merge!(originating_state: {
-        order: "originating_states_people.name AS originating_state_order_statement",
-        order_alias: "originating_state_order_statement"
-      })
-      .merge!(originating_flock: {
-        order: "originating_flocks_people.name AS originating_flock_order_statement",
-        order_alias: "originating_flock_order_statement"
-      })
-  end
+    self.additional_participant_includes += [
+      originating_flock: :translations,
+      originating_state: :translations
+    ]
 
-  def entries_scope
-    super
-      # rubocop:todo Layout/LineLength
-      .joins("LEFT JOIN groups originating_flocks_people ON originating_flocks_people.id = people.originating_flock_id LEFT JOIN group_translations translations_originating_flocks ON translations_originating_flocks.group_id = originating_flocks_people.id")
-      # rubocop:enable Layout/LineLength
-      # rubocop:todo Layout/LineLength
-      .joins("LEFT JOIN groups originating_states_people ON originating_states_people.id = people.originating_state_id LEFT JOIN group_translations translations_originating_states ON translations_originating_states.group_id = originating_states_people.id")
-    # rubocop:enable Layout/LineLength
+    sort_mappings_with_indifferent_access.merge!(
+      originating_state: {
+        order: "originating_states_people.name AS originating_state_order_statement",
+        order_alias: "originating_state_order_statement",
+        joins: [
+          "LEFT JOIN groups originating_states_people " \
+          "ON originating_states_people.id = people.originating_state_id " \
+          "LEFT JOIN group_translations translations_originating_states " \
+          "ON translations_originating_states.group_id = originating_states_people.id"
+        ]
+      },
+      originating_flock: {
+        order: "originating_flocks_people.name AS originating_flock_order_statement",
+        order_alias: "originating_flock_order_statement",
+        joins: [
+          "LEFT JOIN groups originating_flocks_people " \
+          "ON originating_flocks_people.id = people.originating_flock_id " \
+          "LEFT JOIN group_translations translations_originating_flocks " \
+          "ON translations_originating_flocks.group_id = originating_flocks_people.id"
+        ]
+      }
+    )
   end
 end
